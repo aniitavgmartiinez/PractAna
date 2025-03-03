a2595435fa92c843a755088b5fbb186a524e633a
aniitavgmartiinez <anavgmartinez@gmail.com>
2025-03-02 23:42:25 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+export default function App() {
+  return (
+    <div className="flex  justify-center items-center bg-pink-800 text-pink-100 h-screen">
+      <div
+        id="Card"
+        className="flex flex-col justify-end w-52 h-64 p-4 relative  bg-fuchsia-100 rounded-md shadow-lg shadow-lime-300 "
+      >
+        <div className="bg-blue-400 w-44 h-24 absolute -top-5 rounded-lg shadow-lg text-transparent">
+          .
+        </div>
+        <h1 className="text-lime-600">Wolcome</h1>
+        <p className="text-lime-800">
+          Una plataforma digital que te pemitira interactuar con tu tienda de
+          ropa
+        </p>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
+      </div>
+    </div>
+  );
+}
