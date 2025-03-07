56200e3b533d6420fa3640f38e14abcead437399
martinezgarcias690 <martinezgarcias690@gmail.com>
2025-03-07 20:01:47 +0000

    log in

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ export default function App() {
 
   const navigate=useNavigate()
   return (
-    <div style={{backgroundImage:`url(${Perro})`}} className='w-scren h-screen bg-contain bg-no-repeat object-contain '  >
-      <div className='bg-pink-300 w-30% h-50% absolute right-10 top-10 border border-amber-700 rounded-md shadow-md'>
-      <form className='p-50 m-50'>
-        <p className='font-bold'>Nombre:</p>
-        <input type="text" name="name" />
-        <p className='font-bold'>Clave</p>
-        <input type="password" value="Submit"/>
-        <hr className=''/>
-        <button onClick={()=>navigate('/Carta')} className="bg-pink-500 hover:bg-pink-400 text-white font-bold py-2 px-4 rounded">
+    <div style={{backgroundImage:`url(${Perro})`}} className='flex  justify-center items-center w-scren h-screen bg-contain bg-no-repeat object-contain '  >
+      <div className='divide-transparent flex justify-center items-center w-1/4 h-2/4 border border-pink-600 rounded-md shadow-md'>
+      <p className='font-bold'>Bienvenido</p>
+      <form className='flex flex-col justify-end'>
+        <p className='font-bold pb-4 text-teal-700'>Nombre:</p>
+        <input type="text" name="name" className='border border-pink-600 rounded-md shadow-md'/>
+        <p className='font-bold py-4 text-teal-700'>Clave</p>
+        <input type="password" name='' className='border border-pink-600 rounded-md shadow-md'/>
+        <hr className='h-px my-4 '/>
+        <button onClick={()=>navigate('/Carta')} className="bg-pink-600 hover:bg-pink-500 text-white font-bold py-2 px-4 rounded">
           Entrar
         </button>
       </form>
+      <p>Lina Top Shine</p>
       </div>
     </div>
   )
