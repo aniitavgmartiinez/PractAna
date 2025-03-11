be6636dbd74072b8d549280ed8e801f516470b7b
martinezgarcias690 <martinezgarcias690@gmail.com>
2025-03-11 19:31:33 +0000

    [REFACTOR]

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ export default function App() {
   const navigate=useNavigate()
   return (
     <div style={{backgroundImage:`url(${Perro})`}} className='flex  justify-center items-center w-scren h-screen bg-contain bg-no-repeat object-contain'>
-      <div className='divide-transparent flex justify-center items-center w-auto h-auto border border-pink-600 rounded-md shadow-md'>
+      <div className='divide-transparent flex justify-center items-center w-auto h-auto border-2 border-pink-600 rounded-md shadow-md'>
       <form className='flex flex-col justify-end p-10'>
       <p className='font-bold text-lg text-center bg-gradient-to-br from-lime-400 via-teal-300 to-pink-500 text-white p-2 border-transparent rounded-lg'>Bienvenido a<br/>LINA TOPSHINE</p>
-        <p className='font-bold pb-4 pt-8 text-teal-700'>Nombre:</p>
+        <p className='font-bold pb-4 pt-8 text-teal-700'>Nombre de Usuario:</p>
         <input type="text" name="name" className='p-2 border-2 border-pink-600 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-lime-600 focus:border-transparent ...">
   '/>
-        <p className=' font-bold py-4 text-teal-700'>Clave:</p>
+        <p className=' font-bold py-4 text-teal-700'>Contraseña:</p>
         <input type="password" name='' className='p-2 border-2 border-pink-600 rounded-md shadow-md  focus:outline-none focus:ring-2 focus:ring-lime-600 focus:border-transparent ...">
   ">
   '/>
--- src/pages/Carta/index.tsx
+++ src/pages/Carta/index.tsx
@@ -4,16 +4,30 @@ export default function Carta() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex  justify-center items-center bg-pink-200 h-screen">
-      
+    <div className="grid justify-items-stretch bg-pink-200 h-screen col-2">
+      <div className="flex justify-self-start w-40 max-h-auto text-white bg-pink-800 p-4">
+        <header>
+          <nav>
+            <ul className="menu">
+            <li><a href="#" title="Link 1">Inicio</a></li>
+            <li><a href="#" title="Link 2">..</a></li>
+            <li><a href="#" title="Link 3">.</a></li>
+            <li><a href="#" title="Link 4">.</a></li>
+            <li><a href="#" title="Link 5">Cerrar Sesion</a></li>
+            </ul>
+          </nav>
+        </header>
+      </div>
       <div
         id="Card"
-        className="flex flex-col justify-end w-52 h-64 p-4 relative  bg-yellow-200 rounded-md shadow-lg shadow-lime-300 "
+        className="flex flex-col justify-end justify-self-center w-52 h-64 p-4 relative bg-yellow-200 rounded-md shadow-lg shadow-lime-300"
       >
         <div className="bg-pink-300 border border-pink-600 w-44 h-24 absolute -top-5 rounded-lg shadow-lg text-transparent">
           .
         </div>
-        <h1 className="text-lime-900 text-xl font-bold">Bienvenido</h1>
+        <p className="text-lime-900 text-xl font-bold">
+          Bienvenido
+        </p>
         <p className="text-black">
           Una plataforma digital de tu tienda de ropa
         </p>
